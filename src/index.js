9b4c114d3b0ff329fb6f3075f9d05750cae4d689
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-03-01 10:38:51 +0100

    calculator

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-// Say hello let'us build a calculator
-alert("Hello there, let'us build a calculator");
+//intro
+alert(`Welcome to my simple Calculator! 
+You can add, subtract, multiply, divide and many more. 
+Enjoy! 😀`);
+
+//ask user for two numbers
+let number1 = Number(prompt('Enter your first number'));
+let number2 = Number(prompt('Enter your second number'));
+
+while (isNaN(number1) || isNaN(number2)) {
+  number1 = Number(prompt('Enter your first number'));
+  number2 = Number(prompt('Enter your second number'));
+}
+
+//ask user for the type of operation
+let operation = prompt('Choose the operation: +, -, *, /, ^, % (remainder)');
+
+let result;
+if (operation === '+') {
+  result = number1 + number2;
+} else if (operation === '-') {
+  result = number1 - number2;
+} else if (operation === '*') {
+  result = number1 * number2;
+} else if (operation === '/') {
+  result = number1 / number2;
+} else if (operation === '^') {
+  result = Math.pow(number1, number2);
+} else if (operation === '%') {
+  result = number1 % number2;
+} else {
+  result = 'Invalid operation';
+}
+
+//Display the result to the user
+alert(`The result of your operation is: ${result}`);
